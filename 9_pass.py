57e7f07892498ea227182276e5f119a13ab3fae6
saurav-fusemachines <saurav.karki@fusemachines.com>
2022-09-06 13:29:00 +0545

    pass statement

---
+++ 9_pass.py
@@ -0,0 +1,13 @@
+'''
+The pass statement is used as a placeholder for future code.
+
+When the pass statement is executed, nothing happens, but you avoid getting an error when empty code is not allowed.
+
+Empty code is not allowed in loops, function definitions, class definitions, or in if statements.
+'''
+
+name= input("Enter you name: ")
+if "xyz" in name:
+    pass  
+
+print(name)
