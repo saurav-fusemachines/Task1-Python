b91be4d788026a1452830b72f0fbda192b844c6a
saurav-fusemachines <saurav.karki@fusemachines.com>
2022-09-06 14:43:15 +0545

    inbuilt_functions

---
+++ 11_inbuilt_functions.py
@@ -0,0 +1,11 @@
+'''
+A built-in function is a function that is already available in a programming language, 
+application, or another tool that can be accessed by end users
+'''
+#Some bilt in function in pythons are: pow(), length(),min(),max() etc.
+
+a=10;b=12
+print(f"The max number is: {max(a,b)}")
+print()
+
+print(f"The square of 2 is {pow(5,2)}")
