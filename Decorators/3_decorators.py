ef9a3834176c95c978bcb697f38824b232a63e2b
saurav-fusemachines <saurav.karki@fusemachines.com>
2022-09-07 18:15:36 +0545

    making decorator using @

---
+++ Decorators/3_decorators.py
@@ -0,0 +1,30 @@
+# def printer():
+#     print("Hello world.")
+
+# def display_info(func):
+
+#     def inner():
+#         print("Executing",func.__name__,"function")
+#         func()
+#         print("Finished execution.")
+#     return inner
+
+   
+# decorated_func = display_info(printer)
+# decorated_func()
+
+            #OR
+
+# def display_info(func):
+
+    def inner():
+        print("Executing",func.__name__,"function")
+        func()
+        print("Finished execution.")
+    return inner
+
+@display_info
+def printer():
+    print("Hello world.")
+
+printer()
