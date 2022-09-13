c23975e4a6fe636f88d321a064c37dff892a10c6
saurav-fusemachines <saurav.karki@fusemachines.com>
2022-09-13 09:24:44 +0545

    load json file using python

---
+++ Files/6_read_json_file2.py
@@ -0,0 +1,25 @@
+
+# Python program to read
+# json file
+  
+  
+import json
+  
+# Opening JSON file
+f = open("d:\Fusemachines\Python\Files\data.json")
+  
+# returns JSON object as 
+# a dictionary
+data = json.loads(f.read())
+  
+# Iterating through the json
+# list
+print("\nPlayer Details:")
+for i in data['player_details']:
+    print(i)
+
+print("\nMatch Schedule:")
+for i in data['match_schedule']:
+   print(i)
+# Closing file
+f.close()
