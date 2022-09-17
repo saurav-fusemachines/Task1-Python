436bf94f23e9c8c2bc62a6d260e938667522d30c
saurav-fusemachines <saurav.karki@fusemachines.com>
2022-09-17 16:18:54 +0545

    Chapter 4 : Codes of Chapter 4 from Data Camp

---
+++ DataCamp/24_delete_recors.py
@@ -0,0 +1,21 @@
+# Import delete, select
+from sqlalchemy import delete, select,create_engine,metadata,Table
+engine = create_engine('postgresql+psycopg2://student:datacamp@postgresql.csrrinzqubik.us-east-1.rds.amazonaws.com:5432/census')
+connection = engine.connect()
+
+census = Table('census',metadata,autoload =True,autoload_with = engine)
+
+# Build a statement to empty the census table: stmt
+delete_stmt = delete(census)
+
+# Execute the statement: results
+results = connection.execute(delete_stmt)
+
+# Print affected rowcount
+print(results.rowcount)
+
+# Build a statement to select all records from the census table : select_stmt
+select_stmt = select([census])
+
+# Print the results of executing the statement to verify there are no rows
+print(connection.execute(select_stmt).fetchall())
---
+++ DataCamp/25_deleting_specific_recors.py
@@ -0,0 +1,28 @@
+from sqlalchemy import delete, select,create_engine,metadata,Table,and_,func
+engine = create_engine('postgresql+psycopg2://student:datacamp@postgresql.csrrinzqubik.us-east-1.rds.amazonaws.com:5432/census')
+connection = engine.connect()
+
+census = Table('census',metadata,autoload =True,autoload_with = engine)
+# Build a statement to count records using the sex column for Men ('M') age 36: count_stmt
+count_stmt = select([func.count(census.columns.sex)]).where(
+    and_(census.columns.sex == 'M',
+         census.columns.age == 36)
+)
+
+# Execute the select statement and use the scalar() fetch method to save the record count
+to_delete = connection.execute(count_stmt).scalar()
+
+# Build a statement to delete records from the census table: delete_stmt
+delete_stmt = delete(census)
+
+# Append a where clause to target Men ('M') age 36: delete_stmt
+delete_stmt = delete_stmt.where(
+    and_(census.columns.sex == 'M',
+         census.columns.age == 36)
+)
+
+# Execute the statement: results
+results = connection.execute(delete_stmt)
+
+# Print affected rowcount and to_delete record count, make sure they match
+print(results.rowcount, to_delete)
